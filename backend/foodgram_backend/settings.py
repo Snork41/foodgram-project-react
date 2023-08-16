97633bd97b1e22dba93a16f4f5250736d9bde984
Максим Давлеев <davleevkam@gmail.com>
2023-08-16 20:29:48 +0300

    After_first_review

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '123')
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
