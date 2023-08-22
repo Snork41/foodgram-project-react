f91a2b9beac06cd6f6eae9d8597ba1d4c1f05623
Максим Давлеев <davleevkam@gmail.com>
2023-08-22 14:17:57 +0300

    PEP8

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -63,14 +63,14 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('POSTGRES_DB', 'django'),
-       'USER': os.getenv('POSTGRES_USER', 'django'),
-       'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-       'HOST': os.getenv('DB_HOST', ''),
-       'PORT': os.getenv('DB_PORT', 5432)
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }
 }
 
 
